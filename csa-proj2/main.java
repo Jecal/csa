c5252474e5737ae4a640b0c165f6a0cd74fd0237
Jullian <117240843+Jecal@users.noreply.github.com>
2023-10-10 18:42:50 -0400

    Update main.java
    update 1

--- csa-proj2/main.java
+++ csa-proj2/main.java
@@ -1,9 +1,19 @@
+import java.util.Random;
+
 public class main
 {
 	public static void main(String[] args) {
 		// test case ints: 7861, 8191, 7644, 5813
-        studentid stud = new studentid(7644);
+		Random rand = new Random();
+		int rand_int = rand.nextInt(7862);
+		System.out.println("ID generated: " + rand_int);
+		
+		studentid stud = new studentid(rand_int);
 		System.out.println(stud.getID());
+		System.out.println(stud.getGrade());
+		System.out.println(stud.getFirstN());
+		System.out.println(stud.getLastN());
+		System.out.println(stud.getIsMale());
 		stud.print();
 	}
 }
