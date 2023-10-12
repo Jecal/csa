a8fbcd4f4f491db7b42fb6cb10836f8e71da702d
Jullian <117240843+Jecal@users.noreply.github.com>
2023-10-11 22:52:58 -0400

    Update main.java
    includes randint gen and reverse functionality

--- csa-proj2/main.java
+++ csa-proj2/main.java
@@ -1,14 +1,13 @@
 import java.util.Random;
 
-public class main
-{
+public class main {
 	public static void main(String[] args) {
 		// test case ints: 7861, 8191, 7644, 5813
-		Random rand = new Random();
-		int rand_int = rand.nextInt(7862);
-		System.out.println("ID generated: " + rand_int);
-		
-		studentid stud = new studentid(rand_int);
+		// Random rand = new Random();
+		// int rand_int = rand.nextInt(7862);
+		// System.out.println("ID generated: " + rand_int);
+
+		studentid stud = new studentid('W', 'N', true, 3);
 		System.out.println(stud.getID());
 		System.out.println(stud.getGrade());
 		System.out.println(stud.getFirstN());
