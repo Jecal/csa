1846226ecd3e3e0d8cb544472746331cf28a35b4
Jullian <117240843+Jecal@users.noreply.github.com>
2023-10-28 21:16:37 -0400

    Update main.java

--- csa-proj2/main.java
+++ csa-proj2/main.java
@@ -1,4 +1,6 @@
+import java.util.Arrays;
 import java.util.Random;
+import java.util.*;
 
 public class main {
 	public static void main(String[] args) {
@@ -7,13 +9,39 @@ public class main {
 		// int rand_int = rand.nextInt(7862);
 		// System.out.println("ID generated: " + rand_int);
 
-		studentid stud = new studentid('W', 'N', true, 3);
-		// studentid stud2 = new studentid(7644);
-		System.out.println(stud.getID());
-		System.out.println(stud.getGrade());
-		System.out.println(stud.getFirstN());
-		System.out.println(stud.getLastN());
-		System.out.println(stud.getIsMale());
-		stud.print();
+		studentid stud1 = new studentid('W', 'N', true, 2);
+		studentid stud2 = new studentid('W', 'T', true, 1);
+		// System.out.println(stud1.getID());
+		// System.out.println(stud1.getGrade());
+		// System.out.println(stud1.getFirstN());
+		// System.out.println(stud1.getLastN());
+		// System.out.println(stud1.getIsMale());
+		// stud1.print();
+		// stud2.print();
+		// stud1.compareTo(stud2);
+
+		/* sample code for testing */
+		// Declare an array of 20 StudentIDs
+		studentid[] array = new studentid[20];
+		for (int i = 0; i < array.length; i++) {
+			array[i] = new studentid((int) (Math.random() * 8192)); // generate random IDs
+		}
+
+		// import java.util.* required at the top to import Arrays library
+		Arrays.sort(array); // sorts array by whatever is specified in compareTo
+
+		// Print out the sorted array
+		for (int i = 0; i < array.length; i++) {
+			array[i].print();
+		}
+		/* sample code for testing */
+
+		int comp = stud1.compareTo(stud2);
+		if (comp == 1)
+			System.out.println("dudes older");
+		else if (comp == 0)
+			System.out.println("dudes are the same");
+		else
+			System.out.println("nah");
 	}
 }
