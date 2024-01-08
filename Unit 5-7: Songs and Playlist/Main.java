fbddd1d5b31c0a41938f7c728692dd5706a39a0c
Jullian <117240843+Jecal@users.noreply.github.com>
2024-01-08 12:18:12 -0500

    Update Main.java

--- Unit 5-7: Songs and Playlist/Main.java
+++ Unit 5-7: Songs and Playlist/Main.java
@@ -2,29 +2,39 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-
-        Songs drunker = new Songs("drunker", "Fennel", "4:34");
-        Songs dreaming = new Songs("dreaming", "Fennel", "3:43");
-        Songs Sunday = new Songs("Sunday", "Fennel", "3:37");
+        
+        Songs infinite = new Songs("infinitesimal", "ivri", "2:42");
+        Songs nowayout = new Songs("no way out", "ivri", "1:53");
+        Songs sry = new Songs("Sorry for my late reply", "MEMI", "3:31");
+        Songs rain = new Songs("雨、キミを連れて", "EGOIST", "4:15");
+        
+        Playlist noimgood = new Playlist();
+        noimgood.addTrack(infinite);
+        noimgood.addTrack(nowayout);
+        noimgood.addTrack("I'm Just Like That", "YONLAPA",  "3:27");
+        noimgood.addTrack(rain);
+        
+        noimgood.sort();
+        noimgood.print();
+        
+        System.out.println(Playlist.formatTime(noimgood.getRuntime()));
         
         /*
-         * System.out.println(dreaming.getTitle());
-         * System.out.println(Sunday.getArtist());
-         * System.out.println(drunker.getRuntime());
-         */
-
+        System.out.println("//////////////////////////");
+        */
+        
         /*
-         * System.out.println(drunker.getRuntime() + dreaming.getRuntime() +
-         * Sunday.getRuntime());
-         */
-
-        Playlist slowDown = new Playlist();
-        slowDown.addTrack(drunker);
-        slowDown.addTrack(dreaming);
-        slowDown.addTrack(Sunday);
-        // System.out.println(slowDown.getRuntime());
-        slowDown.sort();
-        slowDown.print();
-        System.out.println(slowDown.formatTime());
+        Playlist mySongs = new Playlist();
+        Songs Alive = new Songs ("Alive 2007 Concert", "Daft Punk", "1:24:06");
+        Songs eden = new Songs ("In-A-Gadda-Da-Vida", "Iron Butterfly", "17:03");
+        mySongs.addTrack(eden);
+        mySongs.addTrack("Hard Times", "The Jetzons", "172");
+        mySongs.addTrack("Harder Better Faster Stronger", "Daft Punk", "3:42");
+        mySongs.addTrack("Harder Better Faster Stronger", "Pomplamoose", "4:21");
+        mySongs.addTrack("Hikari", "Utada Hikaru", "4:01");
+        mySongs.addTrack(Alive);
+        mySongs.print();
+        System.out.println(Playlist.formatTime(mySongs.getRuntime()));
+        */
     }
 }
