0da6ccf3bac2c7d86ca832db9aa04fe0271656a7
Jullian <117240843+Jecal@users.noreply.github.com>
2024-02-08 08:26:23 -0500

    Create Horse.java

---
+++ FRQ Review/2012FRQ/Horse.java
@@ -0,0 +1,14 @@
+public class Horse {
+    private String tag;
+    private int weight;
+    public Horse(String name, int val) {
+        tag = name;
+        weight = val;
+    }
+    public String getName() {
+        return tag;
+    }
+    public int getWeight() {
+        return weight;
+    }
+}
